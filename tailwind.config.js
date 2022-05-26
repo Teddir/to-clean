eed1c8fc36141a2f9d42c3105eb6c05be1c07eee
Teddir <ferryaja20000@gmail.com>
2022-05-26 22:44:00 +0700

    first push proejct

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      'primary': "#12141E",
+      'trans1': "#252525",
+      'trans2': "#00020C",
+      'trans3': "#000",
+      'white': '#ffffff',
+      'white-icon': "#D9D9D9",
+      'colorImage': "#030304",
+      'colorButton': "#12141E"
+    },
+    extend: {
+      container: {
+        padding: '1rem',
+      },
+    },
+  },
+  plugins: [],
+}

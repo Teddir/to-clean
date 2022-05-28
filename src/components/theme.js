55b0d8c200d668384e7e82098bdbc062cc36a937
Teddir <ferryaja20000@gmail.com>
2022-05-28 09:01:52 +0700

    update theme

---
+++ src/components/theme.js
@@ -0,0 +1,15 @@
+import { Component } from "react"
+
+class Button extends Component {
+  render() {
+    const texts = this.props.texts
+    const tulisan = texts ? texts : "isi text"
+    return (
+      <div>
+        <a href="https://tokorame.co.id/" type="_parent" className="bg-pink-300 py-2 px-12 full cursor-default">{tulisan}</a>
+      </div>
+    )
+  }
+}
+
+export default Button

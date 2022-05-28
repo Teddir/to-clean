2012500fdc53743e06ce7b8f7d4475d03bc52689
Teddir <ferryaja20000@gmail.com>
2022-05-29 00:51:43 +0700

    update tailwind & logo web

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,20 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    colors: {
-      'primary': "#12141E",
-      'trans1': "#252525",
-      'trans2': "#00020C",
-      'trans3': "#000",
-      'white': '#ffffff',
-      'white-icon': "#D9D9D9",
-      'colorImage': "#030304",
-      'colorButton': "#12141E"
-    },
     extend: {
+      colors: {
+        'primary': "#12141E",
+        'trans1': "#252525",
+        'trans2': "#00020C",
+        'trans3': "#000",
+        'white': '#ffffff',
+        'white-icon': "#D9D9D9",
+        'colorImage': "#030304",
+        'colorButton': "#12141E",
+        'backgroundWeb':"#E5E5E5",
+        'border':"#4A4A55",
+        'blueMuda': "#E7ECFC"
+      },
       container: {
         padding: '1rem',
       },

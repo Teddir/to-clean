dd1402e921f6f2520d876bb5b5de930ba5821542
Teddir <ferryaja20000@gmail.com>
2022-05-30 21:43:47 +0700

    update route

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes,
+  Route, } from "react-router-dom";
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Beranda from './private/Beranda';
+import StepOne from './private/StepOne';
+import StepTwo from './private/StepTwo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Beranda/>
-    {/* <App /> */}
+    <BrowserRouter>
+      {/* <Beranda/>
+      <App /> */}
+      <Routes>
+      <Route path="/" element={<Beranda />} />
+      <Route path="stepone" element={<StepOne />} />
+      <Route path="steptwo" element={<StepTwo />} />
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 

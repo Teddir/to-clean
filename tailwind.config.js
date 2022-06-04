3c698b20ec7a1d240c2c1665d783eed2d22d2c99
Teddir <ferryaja20000@gmail.com>
2022-06-04 23:39:19 +0700

    update screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      'xs': {'min': '200px', 'max': '520px'},
+      'sm': {'min': '520px', 'max': '767px'},
+      ...defaultTheme.screens,
+    },
     extend: {
       colors: {
         'primary': "#12141E",
@@ -16,7 +22,8 @@ module.exports = {
         'backgroundWeb':"#E5E5E5",
         'border':"#4A4A55",
         'blueMuda': "#E7ECFC",
-        'backgroundUploadImage': "#444444"
+        'backgroundUploadImage': "#444444",
+        'myColor1':"#E7ECFC"
       },
       container: {
         padding: '1rem',

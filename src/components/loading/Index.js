056a8690aec5e5a602d784f18309f9818022a3e4
Teddir <ferryaja20000@gmail.com>
2022-06-11 10:49:41 +0700

    add loading screen

---
+++ src/components/loading/Index.js
@@ -0,0 +1,14 @@
+import React from "react";
+export default function Index () {
+  return (
+    <div className="bg-backgroundWeb min-h-screen w-full flex">
+      <span className="waterfall">
+        <div className="waterfall bg-trans1"/>
+        <div className="waterfall bg-trans1"/>
+        <div className="waterfall bg-trans1"/>
+        <div className="waterfall bg-trans1"/>
+        <div className="waterfall bg-trans1"/>
+      </span>
+    </div>
+  )
+}

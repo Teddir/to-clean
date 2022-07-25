9631fec9b2915d99c0e2afd6824f58aaaf825fff
Teddi_r <ferryaja20000@gmail.com>
2022-07-25 23:36:56 +0700

    dashboard user v1

--- src/private/pages/user/dashboard/index.js
+++ src/private/pages/user/dashboard/index.js
@@ -2,278 +2,149 @@ import React from "react";
 import { useData } from "../../../../components/firebase/DataProvider";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../../../../components/firebase/FirebaseProvider";
-import { Button } from "../../../../components/theme";
+import { Button, Header } from "../../../../components/theme";
+import { useState } from "react";
+
+const defaultImage = "https://img.freepik.com/free-vector/pack-surface-cleaning-products_23-2148534089.jpg?t=st=1658765646~exp=1658766246~hmac=781b8024a1639c455335965922a5182c92a9295d1139b0b6c98a23ef92b06409&w=740"
 
 function Index() {
   const { users, cleans } = useData();
   const navigation = useNavigate();
+  const [hoverCard, setHoverCard] = useState(defaultImage)
+  const [clickFilter, setClickFilter] = useState({
+    status:"all",
+    styling: ["text-blue-500"]
+  })
+  
   return (
-    <div className="max-w-screen-2xl min-h-screen bg-backgroundWeb bg-opacity-30">
-      <div className="w-full px-12 pt-12 pb-12 xs:px-4 xss:px-2">
-        <div className="flex xs:block justify-between items-center">
-          <div className="flex items-center mb-4">
-            <span className="textJudul font-bold font-mono">
-              Hallo {users?.username}&nbsp;&nbsp;
-            </span>
-            <span className="text-[20px] font-bold bg-pink-300 p-2 rounded-tr-xl rounded-bl-xl">
-              80
+    <div className="min-h-screen max-w-screen-2xl bg-white  xl:bg-contain">
+      <div className="xl:px-32 px-4">
+        <Header />
+        <div
+          className="text-center mt-6"
+          style={{
+            textAlign: "-webkit-center",
+          }}
+        >
+          <span className="text-[3rem] capitalize font-Pop font-semibold">
+            your job is very hard you must strong ok!
+          </span>
+          <br />
+          <div className="xl:w-[40%]">
+            <span className="text-[1.4rem]">
+              Design a great experience for your users on the web and mobile
+              platforms with Masud. Feel free to
             </span>
           </div>
-          <div className="flex xs:block items-center">
-            <div className="mr-4 xs:mr-0">
-              <Button
-                label={"Keluar"}
-                mode="outline"
-                onPress={() => {
-                  auth.signOut();
-                  navigation("/");
-                }}
-              />
+        </div>
+
+        <div
+          className="text-center mt-6 flex items-center justify-center cursor-pointer"
+          style={{
+            textAlign: "-webkit-center",
+          }}
+        >
+          <div className="bg-white-icon py-4 px-5 rounded-lg flex justify-between w-[32rem] items-center">
+            <span className="text-[10px] font-Pop">Search your work</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
+          </div>
+        </div>
+
+        <div className="mt-4 mb-2">
+          <div className="flex justify-between items-center py-4">
+            <div>
+              <span className={`text-[16px] font-semibold mr-4 cursor-pointer ${ clickFilter.status === "pending" ? clickFilter.styling : ""}`} onClick={() => setClickFilter({
+                status:"pending",
+                styling: ["text-blue-500"]
+              })}>Pending</span>
+              <span className={`text-[16px] font-semibold mr-4 cursor-pointer ${ clickFilter.status === "approved" ? clickFilter.styling : ""}`} onClick={() => setClickFilter({
+                status:"approved",
+                styling: ["text-blue-500"]
+              })}>Approved</span>
+              <span className="text-[16px] font-semibold mr-4">|</span>
+              <span className={`text-[16px] font-semibold mr-4 cursor-pointer ${ clickFilter.status === "all" ? clickFilter.styling : ""}`} onClick={() => setClickFilter({
+                status:"all",
+                styling: ["text-blue-500"]
+              })}>All</span>
+              <span className="text-[16px] font-semibold mr-4 cursor-pointer text-red-500">Rejected</span>
             </div>
-            <div className="mr-0">
-              <Button
-                label={"Mulai bersih-bersih"}
-                onPress={() => navigation("/tora/user/start")}
-              />
+            <div className="flex">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-8 w-8 mr-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-8 w-8"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"
+                />
+              </svg>
             </div>
           </div>
         </div>
 
-        <div className="lg:w-1/2 pt-4">
-          <span className="lg:text-[20px] md:text-[18px] text-[16px]">
-            Kamu bisa melihat status data yang telah kamu upload sebelumnya dan
-            juga melihat seluruh gambar ruangan yang telah kamu bersihkan.
-          </span>
-        </div>
-        <div className="bg-c0 w-full mt-20 py-4 pl-6">
-          <span className="font-bold lg:text-[20px] md:text-[18px] text-[24px]">
-            Gambar Anda
-          </span>
-        </div>
-        <div className="w-full pt-12 pb-12 xs:px-0 xss:px-0 cursor-pointer">
-          {/* pending */}
-          <>
-            <div className="flex justify-between items-center xss:block">
-              <span className="font-semibold lg:text-[20px] md:text-[18px] text-[22px]  ">
-                Pending
-              </span>
-              <div className="xs:my-4 py-2 px-4 justify-center items-center flex cursor-default border-2 border-trans1 border-opacity-10">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 stroke-2"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path d="M19 9l-7 7-7-7" />
-                </svg>
-                <select className="ml-2 appearance-none text-[16px] bg-transparent focus:ring-opacity-0 focus:ring-1 focus:outline-none focus:border-trans1 focus:border-opacity-50">
-                  <option className="font-bold" value={""}>
-                    Hari Ini ygy
-                  </option>
-                  {[
-                    { no: 0, name: "Senin" },
-                    { no: 1, name: "Selesa" },
-                    { no: 3, name: "Rabu" },
-                    { no: 4, name: "Kamis" },
-                    { no: 5, name: "Jum'at" },
-                    { no: 5, name: "Sabtu" },
-                  ].map((data, index) => {
-                    return (
-                      <option key={index} value={data.name}>
-                        {data.name}
-                      </option>
-                    );
-                  })}
-                </select>
-              </div>
-            </div>
-            <div className="flex flex-col m-auto p-auto">
-              <div className="flex snap-x overflow-x-scroll py-10 hide-scroll-bar">
-                <div className="flex snap-start flex-nowrap">
-                  {cleans
-                    ?.filter((a) => a.status === "pending")
-                    .map((datas, index) => {
-                      return datas?.data?.map((a, b) => {
-                        return (
-                          <div key={b} className="inline-block px-3">
-                            <div
-                              style={{
-                                backgroundImage: `url(${a?.thumbnail})`,
-                                backgroundPosition: "center",
-                                backgroundAttachment: "inherit",
-                                backgroundSize: "cover",
-                                backgroundRepeat: "repeat",
-                              }}
-                              className="w-[22rem] h-[20rem] smm:w-[15rem] smm:h-[15rem] xs:w-[18rem] xs:h-[18rem] xss:w-[17rem] xss:h-[18rem] max-w-xs overflow-hidden rounded-lg shadow-md bg-opacity-30 hover:shadow-xl transition-shadow duration-300 ease-in-out items-center justify-center flex"
-                            >
-                              {!a?.thumbnail && (
-                                <>
-                                  <div className="flex">
-                                    <svg
-                                      xmlns="http://www.w3.org/2000/svg"
-                                      className="h-12 w-24 stroke-1 items-center justify-center"
-                                      fill="none"
-                                      color="#252525"
-                                      viewBox="0 0 24 24"
-                                      stroke="currentColor"
-                                    >
-                                      <path d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                                    </svg>
-                                  </div>
-                                </>
-                              )}
-                            </div>
-                            <h4 className="text-[16px] mt-6 font-bold uppercase">
-                              {a.title}
-                            </h4>
-                          </div>
-                        );
-                      });
-                    })}
-                </div>
-              </div>
-            </div>
-          </>
-          {/* approved */}
-          <>
-            <div className="flex justify-between items-center xss:block mt-12">
-              <span className="font-semibold lg:text-[20px] md:text-[18px] text-[22px]  ">
-                Approved
-              </span>
-              <div className="xs:my-4 py-2 px-4 justify-center items-center flex cursor-default border-2 border-trans1 border-opacity-10">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 stroke-2"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path d="M19 9l-7 7-7-7" />
-                </svg>
-                <select className="ml-2 appearance-none text-[18px] bg-transparent focus:ring-opacity-0 focus:ring-1 focus:outline-none focus:border-trans1 focus:border-opacity-50">
-                  <option className="font-bold" value={""}>
-                    Hari Ini ygy
-                  </option>
-                  {[
-                    { no: 0, name: "Senin" },
-                    { no: 1, name: "Selesa" },
-                    { no: 3, name: "Rabu" },
-                    { no: 4, name: "Kamis" },
-                    { no: 5, name: "Jum'at" },
-                    { no: 5, name: "Sabtu" },
-                  ].map((data, index) => {
-                    return (
-                      <option key={index} value={data.name}>
-                        {data.name}
-                      </option>
-                    );
-                  })}
-                </select>
-              </div>
-            </div>
-            <div className="flex flex-col m-auto p-auto">
-              <div className="flex snap-x overflow-x-scroll py-10 hide-scroll-bar">
-                <div className="flex snap-start flex-nowrap ">
-                  {cleans
-                    ?.filter((a) => a.status === "approved")
-                    .map((datas, index) => {
-                      return datas?.data?.map((a, b) => {
-                        return (
-                          <div key={b} className="inline-block px-3">
-                            <div
-                              style={{
-                                backgroundImage: `url(${a?.thumbnail})`,
-                                backgroundPosition: "center",
-                                backgroundAttachment: "inherit",
-                                backgroundSize: "cover",
-                                backgroundRepeat: "repeat",
-                              }}
-                              className="w-[22rem] h-[20rem] smm:w-[15rem] smm:h-[15rem] xs:w-[18rem] xs:h-[18rem] xss:w-[17rem] xss:h-[18rem] max-w-xs overflow-hidden rounded-lg shadow-md bg-opacity-30 hover:shadow-xl transition-shadow duration-300 ease-in-out items-center justify-center flex"
-                            >
-                              {!a?.thumbnail && (
-                                <>
-                                  <div className="flex">
-                                    <svg
-                                      xmlns="http://www.w3.org/2000/svg"
-                                      className="h-12 w-24 stroke-1 items-center justify-center"
-                                      fill="none"
-                                      color="#252525"
-                                      viewBox="0 0 24 24"
-                                      stroke="currentColor"
-                                    >
-                                      <path d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                                    </svg>
-                                  </div>
-                                </>
-                              )}
-                            </div>
-                            <h4 className="text-[16px] mt-6 font-bold uppercase">
-                              {a.title}
-                            </h4>
-                          </div>
-                        );
-                      });
-                    })}
-                </div>
-              </div>
-            </div>
-          </>
-        </div>
-        {/* semua gambar */}
-        <div className="bg-c0 w-full mt-4 py-4 pl-6">
-          <span className="font-bold lg:text-[20px] md:text-[18px] text-[24px]">
-            Semua Gambar
-          </span>
-        </div>
-        <div className="w-full pt-12 pb-12 xs:px-0 xss:px-0">
-          <>
-            <div className="grid xl:grid-cols-6 lg:grid-cols-6 md:grid-cols-5 smm:grid-cols-4 xs:grid-cols-3 xss:grid-cols-2">
-              {cleans?.map((datas, index) => {
-                return datas.data?.map((a, i) => {
-                  return (
-                    <div
-                      key={i}
-                      className="xl:mb-12 mb-4 items-center md:block lg:block"
-                    >
-                      <div className={"block justify-center w-auto"}>
-                        <div
-                          style={{
-                            backgroundImage: `url(${a?.thumbnail})`,
-                            backgroundPosition: "center",
-                            backgroundAttachment: "inherit",
-                            backgroundSize: "cover",
-                            backgroundRepeat: "repeat",
-                          }}
-                          className="flex h-[12rem] w-[12rem] xl:h-[16rem] xl:w-[16rem] smm:w-[10rem] smm:h-[10rem] xs:w-[9rem] xs:h-[9rem] xss:w-[8rem] xss:h-[8rem] bg-opacity-30 items-center justify-center bg-cover"
-                        >
-                          {!a?.thumbnail && (
-                            <>
-                              <div className="flex">
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  className="h-12 w-24 stroke-1 items-center justify-center"
-                                  fill="none"
-                                  color="#252525"
-                                  viewBox="0 0 24 24"
-                                  stroke="currentColor"
-                                >
-                                  <path d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                                </svg>
-                              </div>
-                            </>
-                          )}
+        <div>
+          <div className="grid xl:justify-items-center xl:grid-cols-4 grid-cols-1">
+            {cleans?.map((datas, index) => {
+              const extData = datas?.data[0]
+              return (
+                <div key={index}>
+                  {/* <h1>{datas?.id}</h1> */}
+                  <div>
+                    <div className="p-6 mb-4 bg-white shadow-md rounded-lg w-[22rem]">
+                      <div
+                      id={index}
+                      onMouseEnter={(e) => {
+                        if (Number(e.target?.id) === index) {
+                          setHoverCard({[index]: extData?.thumbnail})
+                        } 
+                      }}
+                      onMouseLeave={(e) => {
+                        if (Number(e.target?.id) === index) {
+                          setHoverCard({[index]: defaultImage})
+                        }
+                      }}
+                      className={`bg-white rounded-md h-[21rem] bg-cover duration-200`} 
+                      style={{
+                        backgroundImage: `url(${hoverCard[index] ? hoverCard[index] : defaultImage})`,
+                        backgroundRepeat:"no-repeat",
+                        backgroundPosition:"center",
+                        backgroundPositionY:"center",
+                      }}/>
+                      <div className="text-center">
+                        <div className="py-4">
+                          <span className="font-Pop font-semibold capitalize text-lg">{extData?.title}</span>
                         </div>
-                        <label className="lg:text-[18px] md:text-[16px] text-[14px] font-sans text-slate-600">
-                          {a.title}
-                        </label>
+                        <div>
+                          <span className="text-slate-300 hover:text-pink-400 cursor-pointer">Detail</span>
+                          <span className="text-slate-300">{" "}|{" "}</span>
+                          <span className="text-slate-300 hover:text-green-400 cursor-pointer">Download Image</span>
+                        </div>
                       </div>
                     </div>
-                  );
-                });
-              })}
-            </div>
-          </>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
         </div>
       </div>
     </div>

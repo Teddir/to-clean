fd8a14bccefdbb5b9049eccbc66b57a251df1218
Teddi_r <ferryaja20000@gmail.com>
2022-07-31 20:53:23 +0700

    update theme

--- src/components/theme.js
+++ src/components/theme.js
@@ -1,7 +1,8 @@
 /* eslint-disable react/prop-types */
 import React, { useState } from "react";
 import { Component } from "react";
-import logo from "../src/svg/logo.svg";
+import logoWhite from "../src/svg/logoWhite.svg";
+import logoBlack from "../src/svg/logoBlack.svg";
 
 export class ButtonOld extends Component {
   constructor(props) {
@@ -138,12 +139,13 @@ export const Button = ({ onPress, mode, disable, label, icon }) => {
   const borderColor = [disable ? "border-disable" : "border-primary"];
   const textColor = [disable ? "text-border" : modeText];
   const cursorPointer = [disable ? "cursor-not-allowed" : "cursor-pointer"];
-  const styleButton = `py-3 px-6 justify-center items-center border-2 ${borderColor} flex ${cursorPointer} ${bgColor} ${textColor}`;
-  const styleButtonOutline = `py-3 px-6 justify-center items-center border-2 flex ${cursorPointer} ${borderColor} ${textColor}`;
+  const shadow = [disable ? "" : "hover:shadow-2xl"];
+  const styleButton = `py-3 px-6 justify-center items-center border-2 ${borderColor} flex ${cursorPointer} ${bgColor} ${textColor} ${shadow}`;
+  const styleButtonOutline = `py-3 px-6 justify-center items-center border-2 flex ${cursorPointer} ${borderColor} ${textColor} ${shadow}`;
   const nilaiStyle = mode === "outline" ? styleButtonOutline : styleButton;
   return (
     <div
-      className={`${nilaiStyle} xs:mb-4  hover:shadow-2xl duration-500`}
+      className={`${nilaiStyle} xs:mb-4 duration-500`}
       onClick={() => (!disable && onPress ? onPress() : {})}
     >
       {icon === "back" && (
@@ -164,7 +166,7 @@ export const Button = ({ onPress, mode, disable, label, icon }) => {
   );
 };
 
-export const SideBarProcess = ({ numberProcess }) => {
+export const SideBarProcess = ({ numberProcess, title }) => {
   const process = [
     { id: 1, name: "Proses 1" },
     { id: 2, name: "Proses 2" },
@@ -175,25 +177,41 @@ export const SideBarProcess = ({ numberProcess }) => {
     <div className="w-[80%] lg:w-[90%] mt-24">
       <div className="h-[50%] cursor-default">
         <div className="pb-8">
-          {process?.map((datas, index) => {
-            const myStyle = [
-              numberProcess >= index + 1
-                ? "text-primary font-bold textResponsive"
-                : "text-white font-bold textResponsive",
-            ];
-            const spanStyle = [
-              numberProcess >= index + 1
-                ? "p-4 bg-backgroundWeb w-[90%] flex justify-center items-center drop-shadow-lg"
-                : "border-2 border-white opacity-10 p-4 bg-transparent w-[90%] flex justify-center items-center rounded-md",
-            ];
-            return (
-              <div key={index} className={"mb-4"}>
-                <div className={myStyle}>
-                  <span className={spanStyle}>{datas?.name}</span>
+          {numberProcess === 0 ? (
+            <>
+              <div className={"mb-4"}>
+                <div className={`h-[50%]`}>
+                  <span
+                    className={`text-white text-[36px] xl:text-[46px] font-serif font-bold`}
+                  >
+                    {title}
+                  </span>
                 </div>
               </div>
-            );
-          })}
+            </>
+          ) : (
+            <>
+              {process?.map((datas, index) => {
+                const myStyle = [
+                  numberProcess >= index + 1
+                    ? "text-primary font-bold textResponsive"
+                    : "text-white font-bold textResponsive",
+                ];
+                const spanStyle = [
+                  numberProcess >= index + 1
+                    ? "p-4 bg-backgroundWeb w-[90%] flex justify-center items-center drop-shadow-lg"
+                    : "border-2 border-white opacity-10 p-4 bg-transparent w-[90%] flex justify-center items-center rounded-md",
+                ];
+                return (
+                  <div key={index} className={"mb-4"}>
+                    <div className={myStyle}>
+                      <span className={spanStyle}>{datas?.name}</span>
+                    </div>
+                  </div>
+                );
+              })}
+            </>
+          )}
         </div>
       </div>
       <div className="h-[50%] flex bottom-0 pb-12 fixed items-end">
@@ -208,50 +226,65 @@ export const SideBarProcess = ({ numberProcess }) => {
 export const useSnackBar = () => {
   return (
     <div className="absolute top-0 items-center justify-center">
-      <div className="bg-yellow-100 rounded-lg py-5 px-6 mb-3 text-base text-yellow-700 inline-flex items-center w-full" role="alert">
-        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="exclamation-triangle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-          <path fill="currentColor" d="M569.517 440.013C587.975 472.007 564.806 512 527.94 512H48.054c-36.937 0-59.999-40.055-41.577-71.987L246.423 23.985c18.467-32.009 64.72-31.951 83.154 0l239.94 416.028zM288 354c-25.405 0-46 20.595-46 46s20.595 46 46 46 46-20.595 46-46-20.595-46-46-46zm-43.673-165.346l7.418 136c.347 6.364 5.609 11.346 11.982 11.346h48.546c6.373 0 11.635-4.982 11.982-11.346l7.418-136c.375-6.874-5.098-12.654-11.982-12.654h-63.383c-6.884 0-12.356 5.78-11.981 12.654z"></path>
+      <div
+        className="bg-yellow-100 rounded-lg py-5 px-6 mb-3 text-base text-yellow-700 inline-flex items-center w-full"
+        role="alert"
+      >
+        <svg
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="fas"
+          data-icon="exclamation-triangle"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 576 512"
+        >
+          <path
+            fill="currentColor"
+            d="M569.517 440.013C587.975 472.007 564.806 512 527.94 512H48.054c-36.937 0-59.999-40.055-41.577-71.987L246.423 23.985c18.467-32.009 64.72-31.951 83.154 0l239.94 416.028zM288 354c-25.405 0-46 20.595-46 46s20.595 46 46 46 46-20.595 46-46-20.595-46-46-46zm-43.673-165.346l7.418 136c.347 6.364 5.609 11.346 11.982 11.346h48.546c6.373 0 11.635-4.982 11.982-11.346l7.418-136c.375-6.874-5.098-12.654-11.982-12.654h-63.383c-6.884 0-12.356 5.78-11.981 12.654z"
+          ></path>
         </svg>
         A simple warning alert - check it out!
       </div>
     </div>
-  )
-}
+  );
+};
 
-export const Header = ({
-  labels,
-  actions
-}) => {
-  const [label, otherLabel] = typeof labels === "string" ? [labels] : labels
-  const [action, otherAction] = typeof actions === "function" ? [actions] : actions
+export const Header = ({ labels, actions, logoColor }) => {
+  const [label, otherLabel] = typeof labels === "string" ? [labels] : labels;
+  const [action, otherAction] =
+    typeof actions === "function" ? [actions] : actions;
   return (
     <div className="w-full xss:px-4 ">
       <div className="flex justify-between items-center">
         <img
-          src={logo}
+          src={logoColor === "black" ? logoBlack : logoWhite}
           className="App-logo h-16 w-16 xl:h-20 xl:w-20"
           alt="logo"
         />
         {labels ? (
           <>
-            <div className="flex">
+            <div className="flex xss:hidden">
               {otherLabel && (
                 <div className="mr-3">
-                  <Button label={otherLabel} mode="outline" onPress={otherAction}/>
+                  <Button
+                    label={otherLabel}
+                    mode="outline"
+                    onPress={otherAction}
+                  />
                 </div>
               )}
               <div>
-                <Button label={label} onPress={action}/>
+                <Button label={label} onPress={action} />
               </div>
             </div>
           </>
-          ) : (
-            <>
-              <div className="h-[1rem] w-[4rem] xl:w-[6rem] bg-gradient-to-r from-white to-white-icon" />
-            </>
-          ) 
-        }
+        ) : (
+          <>
+            <div className="h-[1rem] w-[4rem] xl:w-[6rem] bg-gradient-to-r from-white to-white-icon" />
+          </>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};

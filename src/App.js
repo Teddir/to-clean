e9c442ffbd4ddd17143fb2bb4e2b2be40e41195b
Teddi_r <ferryaja20000@gmail.com>
2022-09-26 23:08:28 +0700

    add SnackbarProvider

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Routers from "./private/routers";
 import FirebaseProvider from "./components/firebase/FirebaseProvider";
 import LandingPage from "./private/LandingPage";
+import { SnackbarProvider } from "notistack";
 
 function App() {
   return (
     <BrowserRouter>
       {/* <Beranda/>
       <App /> */}
-      <FirebaseProvider>
-        <Routes>
-          <Route path="*" element={<Routers />} />
-          <Route path="/" element={<LandingPage />} />
-        </Routes>
-      </FirebaseProvider>
+      <SnackbarProvider maxSnack={3} autoHideDuration={3000}>
+        <FirebaseProvider>
+          <Routes>
+            <Route path="*" element={<Routers />} />
+            <Route path="/" element={<LandingPage />} />
+          </Routes>
+        </FirebaseProvider>
+      </SnackbarProvider>
     </BrowserRouter>
   );
 }
